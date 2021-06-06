907572d257d11586ab32c29261a42f77d7a9c2c2
Aesara B <aesara.b@hotmail.com>
2021-06-06 19:40:37 +0930

    Fixed settings page

--- src/options/options.js
+++ src/options/options.js
@@ -1,9 +1,5 @@
 var options = new Options(configs)
 
-for (const el of customs) {
-	el.onchange = e => el.dispatchEvent(new InputEvent('input', {value: e.value}))
-}
-
 const reloadIFrame = () => document.querySelector('iframe').contentWindow.location.reload()
 
 configs.$addObserver(reloadIFrame)
