09194babf2e2398744913dd4412f157469d0e096
Aesara Binder <aesara@mailbox.org>
2023-12-23 14:01:48 +1030

    Themes are more or less useable now

--- src/resources/stylesheet/sassBuild.sh
+++ src/resources/stylesheet/sassBuild.sh
@@ -1,5 +1,7 @@
-sass --no-source-map "./scss/main.scss" "./main.css"
-sass --no-source-map "./scss/popup.scss" "./popup.css"
+for style in ./scss/*.scss; do
+	filename=$(basename $style)
+	sass --no-source-map "$style" "./${filename%.scss}.css"
+done
 
 for style in ./scss/themes/*.scss; do
 	filename=$(basename $style)
